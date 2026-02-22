efab0fc81027d6c31b38b1bc8eb473b3db2f83e4
bkbimal250 <dos.bimal@gmail.com>
2026-02-22 16:07:16 +0530

    some codes updated

--- apps/calllogs/urls.py
+++ apps/calllogs/urls.py
@@ -7,5 +7,7 @@ router.register(r'', CallLogViewSet, basename='calllog')
 
 urlpatterns = [
     path('sync/', DeviceSyncView.as_view(), name='device-sync'),
+    path('stats/', CallLogViewSet.as_view({'get': 'stats'}), name='calllog-stats'),
+    path('bulk_delete/', CallLogViewSet.as_view({'post': 'bulk_delete'}), name='calllog-bulk-delete'),
     path('', include(router.urls)),
 ]
