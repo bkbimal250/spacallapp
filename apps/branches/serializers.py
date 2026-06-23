ad6ceadf727d01b06522113d488a74115d1982bf
Disha online Solution <dishaspaadvisor@gmail.com>
2026-06-23 18:35:38 +0530

    branch serializer fixed state and city

--- apps/branches/serializers.py
+++ apps/branches/serializers.py
@@ -37,6 +37,33 @@ class BranchSerializer(serializers.ModelSerializer):
             'location_area', 'location_area_name',
             'created_at', 'updated_at',
         ]
+        extra_kwargs = {
+            'state': {'required': False, 'allow_blank': True},
+            'city': {'required': False, 'allow_blank': True},
+            'area': {'required': False, 'allow_blank': True},
+        }
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+        location_state = attrs.get('location_state') or getattr(self.instance, 'location_state', None)
+        location_city = attrs.get('location_city') or getattr(self.instance, 'location_city', None)
+        location_area = attrs.get('location_area') or getattr(self.instance, 'location_area', None)
+
+        if location_state:
+            attrs['state'] = location_state.name
+        if location_city:
+            attrs['city'] = location_city.name
+        if location_area:
+            attrs['area'] = location_area.name
+
+        state = attrs.get('state') or getattr(self.instance, 'state', '')
+        city = attrs.get('city') or getattr(self.instance, 'city', '')
+        if not state:
+            raise serializers.ValidationError({'location_state': 'State is required.'})
+        if not city:
+            raise serializers.ValidationError({'location_city': 'City is required.'})
+
+        return attrs
 
 
 class BranchListSerializer(serializers.ModelSerializer):
